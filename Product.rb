150fa76d0d9f300e34c0edab8d9f53035c1c4c80
Ayax50000 <fjlsoto@gmail.com>
2014-01-02 11:53:37 -0600

    methods to search and add products done

--- Product.rb
+++ Product.rb
@@ -2,6 +2,8 @@ require_relative 'Data.rb'
 
 class Product
 
+  @@current_ids = [7]
+
   def initialize(name,price = 0)
     puts @name = name.upcase
     puts @price = price.to_f
@@ -10,33 +12,52 @@ class Product
   def registered?
     answer = false
     $products.each do |product|
-      answer = true if product[0] == @name
+      answer = true if product[1][0] == @name
     end
     answer
   end
 
+  def self.new_id
+    id = 0
+    if @@current_ids.size > 1
+      id = @@current_ids.last
+      @@current_ids.pop
+    else
+      id = @@current_ids[0]
+      @@current_ids[0] += 1
+    end
+    id
+  end
+
   def register(stock = 0)
-    stock.to_i
     if !(registered?)
-      $products << [@name,@price,stock]
+      $products[Product.new_id] = [@name,@price,stock.to_i]
       $success.call
     else
       $failure.call
     end
   end
 
-  def all(param = "")
-    param.upcase
-    list = $products[0]
-    $products.each do |product|
-      if product[0].include? param
-        list << product
-      end
-    end
-    list
+  def self.show(array)
+   puts "#{array[0]} #{array[1]} #{array[2]}"
   end
 
+  def self.all(param = "")
+    param.upcase!
+    Product.show($products[:index])
+    $products.each do |product|
+      if (product[1][0].include? param) && product[1][0]!="NAME"
+        Product.show(product)
+      end
+    end
+  end
+
+  def prepare_sell()
+
+  end
 end
 
 product = Product.new("freezer",500)
-puts product.register
+ Product.all
+product.register
+Product.all
